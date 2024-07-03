78929e3273e53e3b00552b8d35d54040489d1632
Ridin Kakkoth <k.ridin@ymail.com>
2024-07-03 22:06:12 +0530

    updates

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -40,7 +40,7 @@ const handleStartClick = (itemId) => {
       <h1 className="text-2xl text-prime font-bold mb-4">Mock Interviews</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {data?.map(item => (
-          <div key={item.id} className="bg-white shadow-md rounded-lg overflow-hidden">
+          item.listed&&<div key={item.id} className="bg-white shadow-md rounded-lg overflow-hidden">
             <div className="p-4">
               <h2 className="text-xl text-secondary font-semibold mb-2">{item.title}</h2>
               <p className="text-gray-600">Years of Experience: {item.experience}</p>
